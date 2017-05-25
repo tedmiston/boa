4b79cf2874683e4c8523e9a82d942ea6adbd1218
Taylor Edmiston <tedmiston@gmail.com>
2017-05-25 17:22:46 -0400

    Broaden unit test coverage

--- tests/test_boa.py
+++ tests/test_boa.py
@@ -1,29 +1,70 @@
+import unittest
+
+from nose2.tools import params
+
 import boa
 
-def test_camelcase():
-    snake = boa.constrict('someEvent')
-    assert snake == 'some_event'
+@unittest.skip('not handled yet')
+def test_null():
+    output = boa.constrict(None)
+    assert output == ''
+
+def test_blank():
+    output = boa.constrict('')
+    assert output == ''
 
 def test_lowercase():
-    snake = boa.constrict('someevent')
-    assert snake == 'someevent'
+    output = boa.constrict('someevent')
+    assert output == 'someevent'
 
-def test_special_char():
-    snake = boa.constrict('Some Event\'s')
-    assert snake == 'some_events'
+def test_uppercase():
+    output = boa.constrict('SOMEEVENT')
+    assert output == 'someevent'
 
-def test_capital_spaced():
-    snake = boa.constrict('Info Window')
-    assert snake == 'info_window'
+def test_capitalized():
+    output = boa.constrict('Someevent')
+    assert output == 'someevent'
 
-def test_mixed():
-    snake = boa.constrict('trait_ID')
-    assert snake == 'trait_id'
+@params(
+    ('SomeEvent',),
+    ('someEvent',),
+)
+def test_camelcased(string):
+    output = boa.constrict(string)
+    assert output == 'some_event'
+
+@params(
+    ('Some "Events"',),
+    ('Some Event\'s',),
+)
+def test_special_chars(string):
+    output = boa.constrict(string)
+    assert output == 'some_events'
+
+@params(
+    ('INFO WINDOW',),
+    ('Info Window',),
+    ('info Window',),
+    ('Info window',),
+    ('info window',),
+)
+def test_spaced(string):
+    output = boa.constrict(string)
+    assert output == 'info_window'
+
+@params(
+    ('trait_ID'),
+    ('Trait_ID'),
+    ('Trait_id'),
+)
+def test_mixed(string):
+    output = boa.constrict(string)
+    assert output == 'trait_id'
 
 def test_lower_first_letter():
-    snake = boa.constrict('Add to Collection')
-    assert snake == 'add_to_collection'
+    output = boa.constrict('Add to Collection')
+    assert output == 'add_to_collection'
 
 def test_underscored():
-    snake = boa.constrict('context_ip')
-    assert snake == 'context_ip'
+    output = boa.constrict('context_ip')
+    assert output == 'context_ip'
