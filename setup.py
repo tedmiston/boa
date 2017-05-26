177e9144f414064549ca9f7ee88987432999db6d
Taylor Edmiston <tedmiston@gmail.com>
2017-05-26 13:45:02 -0400

    Add setup.py for setuptools

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='boa',
+    version='1.0.0',
+    description='Convert strings to snakecase',
+    long_description=(
+        'Boa is a Python package for constricting strings to a specific '
+        'snakecase format.\n\nWe use it to translate user-defined event names '
+        'into names we can use for database tables and in S3 file names. For '
+        'example, "User Buys Item" becomes "user_buys_item".'
+    ),
+    url='https://github.com/astronomerio/boa',
+    author='Taylor Edmiston',
+    author_email='taylor@astronomer.io',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='boa snakecase astronomer astronomerio',
+    packages=find_packages(exclude=['tests']),
+    install_requires=[],
+)
